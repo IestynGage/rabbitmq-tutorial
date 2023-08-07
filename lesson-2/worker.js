27373f43568b35294bbea6d12ff191fc14bb8310
Iestyn <>
2023-08-07 21:37:07 +0100

    Finish lesson 2

--- lesson-2/worker.js
+++ lesson-2/worker.js
@@ -2,32 +2,32 @@
 var amqp = require('amqplib/callback_api');
 
 amqp.connect({ username: 'user', password:'bitnami'}, function(error0, connection) {
-    if (error0) {
-        throw error0;
+  if (error0) {
+    throw error0;
+  }
+  connection.createChannel(function(error1, channel) {
+    if (error1) {
+      throw error1;
     }
-    connection.createChannel(function(error1, channel) {
-        if (error1) {
-            throw error1;
-        }
+    var queue = 'task_queue';
 
-        var queue = 'task_queue';
-
-        // This makes sure the queue is declared before attempting to consume from it
-        channel.assertQueue(queue, {
-          durable: true
-        });
-        
-        channel.consume(queue, function(msg) {
-          var secs = msg.content.toString().split('.').length - 1;
-        
-          console.log(" [x] Received %s", msg.content.toString());
-          setTimeout(function() {
-            console.log(" [x] Done");
-          }, secs * 1000);
-        }, {
-          // automatic acknowledgment mode,
-          // see ../confirms.html for details
-          noAck: true
-        });
+    channel.assertQueue(queue, {
+      durable: true
     });
+    channel.prefetch(1);
+    console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+    channel.consume(queue, function(msg) {
+      var secs = msg.content.toString().split('.').length - 1;
+
+      console.log(" [x] Received %s", msg.content.toString());
+      setTimeout(function() {
+        console.log(" [x] Done");
+        channel.ack(msg);
+      }, secs * 1000);
+    }, {
+      // manual acknowledgment mode,
+      // see ../confirms.html for details
+      noAck: false
+    });
+  });
 });
